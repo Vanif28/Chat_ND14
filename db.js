d70b6af1eaaf92a6c4ff77f2e5a95a62772bd5e5
Vanif28 <mpar202020@gmail.com>
2025-04-27 15:40:08 +0300

    jyhtrgrfs

--- db.js
+++ db.js
@@ -1,12 +1,45 @@
-let mysql = require("mysql2")
-require("dotenv").config()
+let mysql = require("mysql2");
+require("dotenv").config();
 
 let db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS
-})
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+});
 
-module.exports = db
+let adb = db.promise();
+
+async function getUsers() {
+  try {
+    let [users, fields] = await adb.query("SELECT * FROM User");
+    return users;
+  } catch (err) {
+    throw err.message;
+  }
+}
+
+async function addMessage(content, author_id) {
+  try {
+    let [users, fields] = await adb.query(
+      "Insert into Message1(content, author_id) values(?, ?)",
+      [content, author_id]
+    );
+    return users;
+  } catch (err) {
+    throw err.message;
+  }
+}
+async function getMessages(){
+  try {
+    let [users, fields] = await adb.query(`SELECT m.id, m.content, a.id from Message1 ON m JOIN `);
+    return users;
+  } catch (err) {
+    throw err.message;
+  }
+}
+
+module.exports = {
+  getUsers,
+};
